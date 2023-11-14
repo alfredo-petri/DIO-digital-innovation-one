86717cd86825260ffb41478677a6c7e3d773e103
Alfredo Augusto Petri <alfredo_petri@hotmail.com>
2023-11-13 22:43:50 -0300

    create desafio-escrevendo-as-classes-de-um-jogo

---
+++ bootcamps/ifood/logica-de-programacao/desafio-escrevendo-as-classes-de-um-jogo/index.js
@@ -0,0 +1,10 @@
+class hero {
+    constructor (name, age, type, attack){
+        this.name = name;
+        this.age = age;
+        this.type = type;
+        this.attack = attack;
+    }
+}
+
+let hero = new hero ("Oderfla", 31, "ninja" )
